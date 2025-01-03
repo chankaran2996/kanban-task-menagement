446894a7a12915b6614f5225363772a493c04005
chankaran2996 <chandrasekaran2996@gmail.com>
2025-01-03 17:09:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './componts/Header'
+import Center from './componts/Center'
+
+
+const App = () => {
+  return (
+    <div >
+      <Header/>
+      <Center/>
+    </div>
+  )
+}
+
+export default App
