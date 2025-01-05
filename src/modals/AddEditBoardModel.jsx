0634ec23c39243c1e7e233cdc9e39e8c7c858fd2
chankaran2996 <chandrasekaran2996@gmail.com>
2025-01-05 13:13:56 +0530

    1.4

--- src/modals/AddEditBoardModel.jsx
+++ src/modals/AddEditBoardModel.jsx
@@ -8,8 +8,20 @@ const AddEditBoardModel = ({setBoardModelOpen}) => {
         setBoardModelOpen(false)
     }
   return (
-    <div onClick={close} className='fixed right-0 left-0 top-0 bottom-0 px-2 py-4 overflow-scroll z-50 justify-center items-center flex bg-[#00000080]'>
-      Check
+    <div onClick={close} className='fixed scrollbar-hide right-0 left-0 top-0 bottom-0 px-2 py-4 overflow-scroll z-50 justify-center items-center flex bg-[#00000080]'>
+      <div className='scrollbar-hide overflow-scroll max-h-[95vh] bg-white dark:bg-[#2b2c37] text-black dark:text-white font-bold shadow-md shadow-[#364e7e1a] max-w-md mx-auto w-full px-8 py-8 rounded-xl'>
+        <h3 className='text-lg'>
+            {
+                type === 'edit' ? 'Edit' : 'Add New'
+            }Board Name
+        </h3>
+
+        <div className='mt-8 flex flex-col space-y-3'>
+            <label className='text-sm dark:text-white text-gray-500'>
+                Board Columns
+            </label>
+        </div>
+      </div>
     </div>
   )
 }
